3d6ba39424e6ce31f97aa877be53788c4b416ccc
MD JUHAIAR MANAR CHOWDHURY <89413439+JuhairManar@users.noreply.github.com>
2024-02-25 04:07:16 +0600

    Add files via upload

---
+++ Class 4/script.js
@@ -0,0 +1,26 @@
+var b = document.getElementById("bt");
+
+b.addEventListener('click', function() {
+    var r = document.createElement("tr");
+
+    r.innerHTML = '<td><input type="text"></td>' +
+        '<td><input type="text"></td>' +
+        '<td><input type="text"></td>' +
+        '<td><input type="text"></td>' +
+        '<td><button class="remove" style="background-color: red;">-</button></td>';
+
+    var t = document.querySelector("tbody");
+    t.appendChild(r);
+
+    var removeButton = r.querySelector('.remove');
+    removeButton.addEventListener('click', function() {
+        r.remove();
+    });
+});
+
+
+
+
+
+
+
